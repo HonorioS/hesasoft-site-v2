1a389414fe68c76519dbd1fc294098988053f983
HonorioS <dasilvah77@gmail.com>
2021-09-10 23:13:24 +0100

    first commit

---
+++ _head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="PT">
+<head>
+    <meta name="google-site-verification" content="DVkCHgGdp8ex_Fi65nkaPP30qfxiEgRh0IU4RcSilS4" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <link rel="icon" href="img/logo/favicon.png" type="image/x-icon" />
+    <title>HESASOFT[ Em Construção!]</title>
